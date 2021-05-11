24230a2c22580b0ab4b0c025056657d9da186a55
happyy0801 <happyy0801@naver.com>
2021-05-11 20:26:56 +0900

    z

---
+++ src/components/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="increasing" @click="increment()">
+    increasing
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Layout",
+  props: {
+    methods: {
+      increment() {
+        this.$store.commit("setCount");
+        console.log(this.$store.state.count);
+      },
+    },
+  },
+};
+</script>
+<style scoped></style>
